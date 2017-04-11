727fa999f27132eefcd21ee5dc667ab2dd3e3df1
Navod Bopitiya <s3617221@student.rmit.edu.au>
2017-04-12 02:44:31 +1000

    Player move and test cases

--- EscapeTheBots/src/etb/player/Player.java
+++ EscapeTheBots/src/etb/player/Player.java
@@ -2,35 +2,80 @@
 
 package etb.player;
 
-import java.awt.event.KeyEvent;
-import java.awt.event.KeyListener;
 
 import etb.user.User;
 
+public class Player extends User{
 
-public class Player extends User implements KeyListener {
-	
-	private int[][] position = new int[2][1];
+	private int x;
+	private int y;
 	private int speed;
 	private int score;
 	
+	private final int INITIAL_X_VALUE = 10;
+	private final int INITIAL_Y_VALUE = 10;
+	private final int DEFAULT_SPEED_VALUE = 1;
+
 	public Player(String username, String password) {
 		super(username, password);
+		this.x = INITIAL_X_VALUE;
+		this.y = INITIAL_Y_VALUE;
+		this.score = 0;
+		this.speed = DEFAULT_SPEED_VALUE;
 	}
 
-	@Override
-	public void keyPressed(KeyEvent e) {
-		// TODO Auto-generated method stub
+	public int getX() {
+		return x;
+	}
+
+	public void setX(int x) {
+		this.x = x;
+	}
+
+	public int getY() {
+		return y;
+	}
+
+	public void setY(int y) {
+		this.y = y;
+	}
+
+	public int getSpeed() {
+		return speed;
+	}
+
+	public void setSpeed(int speed) {
+		this.speed = speed;
+	}
+	
+	public boolean move(String direction){
+		if(direction == "north"){
+			setY(--y);
+			return true;
+		}else if(direction == "south"){
+			setY(++y);
+			return true;
+		}else if(direction == "west"){
+			setX(--x);
+			return true;
+		}else if(direction == "east"){
+			setX(++x);
+			return true;
+		}else{
+			return false;
+		}
 		
 	}
-
-	@Override
-	public void keyReleased(KeyEvent e) {
-		// TODO Auto-generated method stub
+	
+	public boolean poisoned(Boolean poison){
+		if(poison){
+			this.speed =  this.speed / 2;
+			return true;
+		}else{
+			return false;
+		}
+		
 		
 	}
-
-	@Override
-	public void keyTyped(KeyEvent e) {}
-
+	
 }
