f3feb3129c04d069031ccc026b663841686b867d
Navod Bopitiya <s3617221@student.rmit.edu.au>
2017-04-11 18:28:56 +1000

    Player updates

--- EscapeTheBots/src/etb/player/Player.java
+++ EscapeTheBots/src/etb/player/Player.java
@@ -2,12 +2,35 @@
 
 package etb.player;
 
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+
 import etb.user.User;
 
-public class Player extends User {
 
+public class Player extends User implements KeyListener {
+	
+	private int[][] position = new int[2][1];
+	private int speed;
+	private int score;
+	
 	public Player(String username, String password) {
 		super(username, password);
 	}
+
+	@Override
+	public void keyPressed(KeyEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void keyReleased(KeyEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void keyTyped(KeyEvent e) {}
 
 }
---
+++ EscapeTheBots/src/etb/player/PlayerTest.java
@@ -0,0 +1,21 @@
+/**
+ * 
+ */
+package etb.player;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+/**
+ * @author Navod Bopitiya
+ *
+ */
+public class PlayerTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
