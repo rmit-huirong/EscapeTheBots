0c20e22867fa2a6c2cca379b03ace9bec12ddcc8
rmit-s3596883-Muhammad-Shamshudin <s3596883@student.rmit.edu.au>
2017-04-14 14:46:03 +1000

    user

---
+++ EscapeTheBots/src/etb/user/LoginTest.java
@@ -0,0 +1,15 @@
+package etb.user;
+
+import static org.junit.Assert.*;
+import org.junit.Before;
+import org.junit.Ignore;
+import org.junit.Test;
+
+public class LoginTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
